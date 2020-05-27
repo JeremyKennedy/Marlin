c549049265174da4751582c7c411b7c3c9532733
Jeremy Kennedy <me@jeremyk.net>
2020-05-27 19:11:36 -0400

    move include to bottom

--- Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_V2_0.h
+++ Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_V2_0.h
@@ -21,9 +21,6 @@
  */
 #pragma once
 
-#define SKR_MINI_E3_V2
-#include "pins_BTT_SKR_MINI_E3.h"
-
 #define BOARD_INFO_NAME "BIGTREE SKR Mini E3 V2.0"
 
 // Release PA13/PA14 (led, usb control) from SWD pins
@@ -53,3 +50,6 @@
   #define Z_HARDWARE_SERIAL  Serial4
   #define E0_HARDWARE_SERIAL Serial4
 #endif
+
+#define SKR_MINI_E3_V2
+#include "pins_BTT_SKR_MINI_E3.h"
