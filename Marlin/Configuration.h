7d98f2ad64f046edb6272e548d92dce0870bc15b
Jeremy Kennedy <me@jeremyk.net>
2020-06-09 13:54:09 -0400

    calibrate pid

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -491,9 +491,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 23.82
+  #define DEFAULT_Ki 2.38
+  #define DEFAULT_Kd 60.04
 
 #endif // PIDTEMP
 
